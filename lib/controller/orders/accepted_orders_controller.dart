0ac24aaa3ef1a4d7979147a8d390deb0c80349dc
haythamalsharany <talsharany@gmail.com>
2023-09-12 04:00:22 -0900

    commit admin app

---
+++ lib/controller/orders/accepted_orders_controller.dart
@@ -0,0 +1,82 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+import '../../core/class/statusrequest.dart';
+import '../../core/functions/handingdatacontroller.dart';
+import '../../core/services/services.dart';
+import '../../data/data_source/remote/orders/accepted_orders_data.dart';
+import '../../data/models/orders_model.dart';
+import 'pindings_orders_controller.dart';
+
+class AcceptedOrdersController extends OrdersController {
+  final AcceptedOrdersData _acceptedOrdersData = AcceptedOrdersData(Get.find());
+  MyServices myServices = Get.find();
+  List<OrdersModel> acceptedOrders = [];
+  StatusRequest statusRequest = StatusRequest.none;
+
+  getAcceptedOrders() async {
+    statusRequest = StatusRequest.loading;
+    update();
+
+    log(' userId --------------------------: ${myServices.sharedPreferences.getString('id')!}');
+    var response = await _acceptedOrdersData
+        .getAcceptedOrders(myServices.sharedPreferences.getString('id')!);
+    statusRequest = handlingData(response);
+
+    log(' acceptedOrders : $response');
+    if (statusRequest == StatusRequest.success) {
+      if (response['status'] == 'success') {
+        acceptedOrders.clear();
+        List dataList = response['data'];
+        acceptedOrders
+            .addAll(dataList.map((e) => OrdersModel.fromJson(e)).toList());
+
+        if (acceptedOrders.isEmpty) {
+          statusRequest = StatusRequest.failure;
+        }
+      } else {
+        statusRequest = StatusRequest.failure;
+      }
+    } else {
+      statusRequest = StatusRequest.failure;
+    }
+    update();
+  }
+
+  @override
+  makeDoneOrder(String orderId, String userId) async {
+    statusRequest = StatusRequest.loading;
+    update();
+    var response = await _acceptedOrdersData.makeDoneOrder(orderId, userId);
+    statusRequest = handlingData(response);
+
+    log(' makeDoneOrder : $response');
+    if (statusRequest == StatusRequest.success) {
+      if (response['status'] == 'success') {
+        Get.rawSnackbar(
+            title: "69".tr, messageText:  Text('71'.tr));
+        getAcceptedOrders();
+      }
+      {
+        statusRequest = StatusRequest.failure;
+      }
+    } else {
+      statusRequest = StatusRequest.failure;
+    }
+
+    update();
+  }
+
+  refreshOrders() {
+    getAcceptedOrders();
+  }
+
+  @override
+  void onInit() {
+    getAcceptedOrders();
+    super.onInit();
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+
+
+import 'core/constants/routes.dart';
+import 'core/middleware/mymiddleware.dart';
+import 'view/screens/auth/forgetpassword/forgetpassword.dart';
+import 'view/screens/auth/forgetpassword/resetpassword.dart';
+import 'view/screens/auth/forgetpassword/success_resetpassword.dart';
+import 'view/screens/auth/forgetpassword/verifycode.dart';
+import 'view/screens/auth/login.dart';
+
+import 'view/screens/home_screen.dart';
+import 'view/screens/language.dart';
+import 'view/screens/orders/archive_orders.dart';
+import 'view/screens/orders/order_details.dart';
+
+List<GetPage<dynamic>>? routes = [
+  GetPage(
+      name: "/", page: () => const Language(), middlewares: [MyMiddleWare()]),
+  GetPage(name: AppRoute.login, page: () => const Login()),
+  GetPage(name: AppRoute.forgetPassword, page: () => const ForgetPassword()),
+  GetPage(name: AppRoute.verfiyCode, page: () => const VerfiyCode()),
+  GetPage(name: AppRoute.resetPassword, page: () => const ResetPassword()),
+  GetPage(
+      name: AppRoute.successResetpassword,
+      page: () => const SuccessResetPassword()),
+  GetPage(name: AppRoute.homeScreen, page: () => const HomeScreen()),
+  GetPage(name: AppRoute.ordersarchive, page: () => const ArchiveOrders()),
+  GetPage(name: AppRoute.ordersdetails, page: () => const OrderDetails()),
+  GetPage(name: AppRoute.lang, page: () => const Language()),
+];
